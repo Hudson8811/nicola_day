5cdd1a4be409a092ce966e9c7b275d2f667baec5
hudson88@yandex.ru <hudson88@yandex.ru>
2021-05-01 15:27:45 +0300

    added about-music page

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
    // $("input[name='phone']").mask(" +7 (999) 999-99-99");
+   const videoControl = $('#play');
 
+   videoControl.on('click', function () {
+      const videoWrapper = $('#video-link');
+      const URL = videoWrapper.data('video-link');
+      const script = `<iframe src="${URL}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+      videoWrapper.append(script);
+      videoWrapper.addClass('active');
+
+   });
 });
 
