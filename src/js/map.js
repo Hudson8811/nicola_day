e0e999ff7e19031c9d6c212b0d568261328e50fc
hudson88@yandex.ru <hudson88@yandex.ru>
2021-05-04 15:41:52 +0300

    refact map

--- src/js/map.js
+++ src/js/map.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 	mapboxgl.accessToken = 'pk.eyJ1IjoiaXZhbm92YW50b24iLCJhIjoiY2tvODV3cGl1MGU2NTJuczI2aHpqcG5vcCJ9.wIC5RbDr4E9-SFpTzoRVew';
 	const URL = "mapbox://styles/ivanovanton/cko8de0830l8117p28w7r65rp";
+	const itemsList = document.querySelector('.map-sidebar>ul');
 
 	var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/ivanovanton/cko8de0830l8117p28w7r65rp',
 	center: [37.673, 55.667],
 	zoom: 15
+	});
+
+	itemsList.addEventListener('mouseover', event => {
+		
 	});
 
 	map.on('click', function(e) {
@@ -22,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	  
 		var popup = new mapboxgl.Popup({ offset: [0, -15] })
 		  .setLngLat(feature.geometry.coordinates)
-		  .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+		  .setHTML('<h3>' + feature.properties.title + '</h3>')
 		  .addTo(map);
 	  });
 
