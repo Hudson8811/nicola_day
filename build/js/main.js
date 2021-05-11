1c6389df01ad1f1a3ebdeb9fe9e2e4e106c16010
hudson88@yandex.ru <hudson88@yandex.ru>
2021-05-11 16:26:20 +0300

    refact styles

--- build/js/main.js
+++ build/js/main.js
@@ -51,21 +51,46 @@ $(document).ready(function () {
    const scheduleHeader = document.getElementById('scheduleHeader');
    const compensator = document.querySelector('.schedule__block-mt');
    const scrollBlock = document.querySelector('.schedule__scroll-block');
+   const headerMobile = document.querySelector('.header-mobile');
+   const main = document.querySelector('main');
+   const header = document.querySelector('header');
 
    if (scheduleHeader) {
-      const toggleScroll = () => {
+      const toggleScroll = (media, mediaTablet) => {
          const scheduleHeaderClientRect = scheduleHeader.getBoundingClientRect();
 
          window.addEventListener('scroll', () => {
-            if (scrollY >= scheduleHeaderClientRect.top) {
-               scheduleHeader.classList.add('schedule__block-fixed');
-               scrollBlock.classList.add('active');
-               compensator.style.marginTop = 43 + 'px';
-            } else if (scrollY < scheduleHeaderClientRect.top && scheduleHeader.classList.contains('schedule__block-fixed')) {
-               scheduleHeader.classList.remove('schedule__block-fixed');
-               scrollBlock.classList.remove('active');
-               compensator.style.marginTop = 0;
+            if (media.matches) {
+               if (scrollY >= scheduleHeaderClientRect.top - 131) {
+                  scheduleHeader.classList.add('schedule__block-fixed');
+                  scrollBlock.classList.add('active');
+                  compensator.style.marginTop = 43 + 'px';
+               } else if (scrollY < scheduleHeaderClientRect.top && scheduleHeader.classList.contains('schedule__block-fixed')) {
+                  scheduleHeader.classList.remove('schedule__block-fixed');
+                  scrollBlock.classList.remove('active');
+                  compensator.style.marginTop = 0;
+               }
             }
+
+            if (mediaTablet.matches) {
+               headerMobile.style.position = scrollY > 0 ? 'fixed' : 'static';
+               header.style.paddingTop = scrollY > 0 ? '67px' : '0';
+
+               if (scrollY >= scheduleHeaderClientRect.top - 67) {
+                  scheduleHeader.classList.add('schedule__block-fixed');
+                  scrollBlock.classList.add('active');
+                  compensator.style.marginTop = 43 + 'px';
+               } else if (scrollY < scheduleHeaderClientRect.top && scheduleHeader.classList.contains('schedule__block-fixed')) {
+                  scheduleHeader.classList.remove('schedule__block-fixed');
+                  scrollBlock.classList.remove('active');
+                  compensator.style.marginTop = 0;
+               }
+            } else {
+               header.style.position = scrollY > 0 ? 'fixed' : 'static';
+               main.style.paddingTop = scrollY > 0 ? '131px' : '0';
+            }
+
+
          });
 
          scrollBlock.addEventListener('scroll', event => {
@@ -84,11 +109,16 @@ $(document).ready(function () {
 
       const checkWidth = () => {
          const media = window.matchMedia("(max-width: 1250px)");
-         if (media.matches) {
-            toggleScroll();
-         }
+         const mediaTablet = window.matchMedia("(max-width: 999px)");
+
+         toggleScroll(media, mediaTablet);
+
+         media.addEventListener('change', toggleScroll);
+         mediaTablet.addEventListener('change', toggleScroll);
       };
    
       checkWidth();
    }
+   
+
 });
